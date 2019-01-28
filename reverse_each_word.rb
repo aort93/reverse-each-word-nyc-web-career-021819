3fcccd4e8c9dec45d8f16988c7a70a2f96fa594c
Alonso Ortega <alonsojortega@gmail.com>
2019-01-28 02:14:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(sentence)
   # words = sentence.split(' ')
   # reverse = []
-  # 
+  #
   # words.each do |word|
   #   new_word = word.reverse()
   #   reverse.push(new_word)
   # end
-  # 
+  #
   # return reverse.join(' ')
 
   sentence.split.collect {|word| word.reverse}.join(" ")
